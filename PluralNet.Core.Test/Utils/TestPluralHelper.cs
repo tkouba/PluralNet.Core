9920e6fc3e90bf0614e84c9a0dbc4c747a1aa6f9
Tomáš Kouba <tomas.kouba@vinci-energies.net>
2024-01-03 14:43:51 +0100

    Tests for PluralHelper and ResourceLoaderExtensions

---
+++ PluralNet.Core.Test/Utils/TestPluralHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluralNet.Providers;
+using PluralNet.Utils;
+
+namespace PluralNet.Core.Test.Utils
+{
+    public class TestPluralHelper
+    {
+        [Theory]
+        [InlineData("en", typeof(OnlyOneProvider))]
+        [InlineData("cs", typeof(CzechProvider))]
+        public void ChoosePluralProvider(string language, Type expected)
+        {
+            Assert.IsType(expected, PluralHelper.GetPluralChooser(language));
+        }
+
+        [Fact]
+        public void ChooseInvariant()
+        {
+            Assert.IsType<OtherProvider>(PluralHelper.GetPluralChooser(System.Globalization.CultureInfo.InvariantCulture));
+        }
+
+    }
+}
